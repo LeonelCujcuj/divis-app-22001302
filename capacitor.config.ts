ee9cbafa2517d4e8a00a9c3092c25fc69eb303b6
LeonelCujcuj <99225995+LeonelCujcuj@users.noreply.github.com>
2023-12-03 17:14:53 -0600

    Frist commit
    commit inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DivisApp_22001302',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
